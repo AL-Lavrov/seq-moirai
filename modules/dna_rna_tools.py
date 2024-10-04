139fcf6fc28b4fa8847bca824d2f9e2985b8338a
Aleksandr Lavrov <Al.i.lavrov@yandex.ru>
2024-10-04 14:09:10 +0300

    Add orginize_data function to dna_rna_tools.py

--- modules/dna_rna_tools.py
+++ modules/dna_rna_tools.py
@@ -1,5 +1,16 @@
 import bases
 
+def orginize_data(*seqs_and_action, **options):
+    print(options)
+    seqs = seqs_and_action[:-1]
+    action = str(seqs_and_action[-1])
+    if is_nucleic_acid(seqs) is False:
+        raise ValueError('Not a nucleic acid')
+    if 'seq_type' not in options:
+        options['seq_type'] = is_dna_or_rna(seqs)
+    return seqs, action, {'seq_type': options['seq_type'],
+        'frame': options['frame']}
+
 
 def is_nucleic_acid(seqs: tuple) -> bool:
     seqs = tuple(seq.upper() for seq in seqs)
@@ -23,11 +34,12 @@ def transcribe_seq(seq: str, seq_type: str) -> str:
         return seq.replace('u', 't').replace('U', 'T')
 
 
-def reverse_seq(seq: str) -> str:
+def reverse_seq(seq, **kwargs) -> str:
     return seq[::-1]
 
 
-def complement_seq(seq: str, seq_type: str) -> str:
+def complement_seq(seq, **options) -> str:
+    seq_type = options['seq_type']
     if seq_type == "rna":
         return seq.translate(str.maketrans(bases.rna_base_pairs))
     if seq_type == "dna":
@@ -36,3 +48,10 @@ def complement_seq(seq: str, seq_type: str) -> str:
 
 def reverse_complement_seq(seq: str, seq_type: str) -> str:
     return reverse_seq(complement_seq(seq, seq_type))
+
+action_map = {'transcribe': transcribe_seq,
+              'reverse': reverse_seq,
+              'reverse_complement': reverse_complement_seq,
+              'complement': complement_seq}
+
+print(orginize_data('GAATAGGGgc', 'uuuaaaa', 'complement'))
--- seq-moirai.py
+++ seq-moirai.py
@@ -1,21 +1,9 @@
-import modules.dna_rna_tools as drt
+import dna_rna_tools as drt
 
-def run_dna_rna_tools(*seqs_and_action, seq_type=None):
-    action = str(seqs_and_action[-1])
-    seqs = seqs_and_action[:-1]
+def run_dna_rna_tools(*seqs_and_action, **options):
+    seqs, action, options = drt.orginize_data(*seqs_and_action, **options)
+    print(options)
+    return [drt.action_map[action](seq, **options) for seq in seqs]
 
-    if drt.is_nucleic_acid(seqs) is False:
-        return None
-    if seq_type is None:
-        seq_type = drt.is_dna_or_rna(seqs)
 
-    if action == 'reverse':
-        processed_seq = [drt.reverse_seq(seq) for seq in seqs]
-    elif action == 'transcribe' and seq_type:
-        processed_seq = [drt.transcribe_seq(seq, seq_type) for seq in seqs]
-    elif action == 'complement' and seq_type:
-        processed_seq = [drt.complement_seq(seq, seq_type) for seq in seqs]
-    elif action == 'reverse_complement' and seq_type:
-        processed_seq = [drt.reverse_complement_seq(seq, seq_type) for seq in seqs]
-
-    return processed_seq[0] if len(processed_seq) == 1 else processed_seq
+print(run_dna_rna_tools('GAAAAGGGgc', 'uuuaTaaa', 'complement', fragme=1))

9c18d6daa0a5e76f305748a31c620b25d5a470a0
Aleksandr Lavrov <Al.i.lavrov@yandex.ru>
2024-10-06 03:31:22 +0300

    Add filter_fastq function to seq moirai

--- seq-moirai.py
+++ seq-moirai.py
@@ -1,9 +1,10 @@
 import modules.dna_rna_tools as drt
-from typing import Union
+from typing import Union, Tuple, Dict
 
 
 def run_dna_rna_tools(*seqs_and_action: str, **options: drt.Options
                       ) -> Union[str, list]:
+    
     seqs, action, options = drt.get_data(*seqs_and_action, **options)
     action_map = {'transcribe': drt.transcribe,
                   'reverse': drt.reverse,
@@ -13,3 +14,11 @@ def run_dna_rna_tools(*seqs_and_action: str, **options: drt.Options
                   'translate': drt.translate}
     processed_seq = [action_map[action](seq, **options) for seq in seqs]
     return processed_seq[0] if len(processed_seq) == 1 else processed_seq
+
+
+def filter_fastq(seqs: Dict[str, Tuple[str]], 
+                 gc_bounds: Union[float, Tuple[float]] = (0, 100), 
+                 length_bounds: Union[float, Tuple[float]] = (0, 2**32), 
+                 quality_bounds: float = 0
+                 ) -> Dict[str, Tuple[str]]:
+    pass
